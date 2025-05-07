7407b7052fefe84e1b78758980feac9f8482c1c0
Kellen Miller <kellen.miller12@gmail.com>
2025-05-07 06:44:48 -0400

    finished simple heap allocation

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,21 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(big_iron::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+extern crate alloc;
 
+use alloc::boxed::Box;
+use alloc::rc::Rc;
+use alloc::vec;
+use alloc::vec::Vec;
 use big_iron::memory::BootInfoFrameAllocator;
-use big_iron::{memory, println};
+use big_iron::{allocator, memory, println};
 use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
+use x86_64::VirtAddr;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::VirtAddr;
-
     println!("Hello World{}", "!");
     big_iron::init();
 
@@ -21,7 +25,30 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    // as before
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
+
     #[cfg(test)]
     test_main();
 
